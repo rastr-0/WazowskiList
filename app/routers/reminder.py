d9f1cf0bf8d1853e7f5fae811b600d8c44cabae5
rastr <romanmilko123@gmail,com>
2024-11-10 14:55:01 +0100

    MessageBroker class + updated reminder routers

--- app/routers/reminder.py
+++ app/routers/reminder.py
@@ -9,76 +9,155 @@ from app.database.database import motor_db
 from app.logs.logging_config import reminder_logger
 # pydantic models
 from app.models.reminder import Reminder
-from app.schemas.reminder import CreateReminder, ReminderResponse
+from app.schemas.reminder import CreateReminder, ReminderResponse, UpdateReminder
 # uitls
 from app.utils.utils import get_current_user
 # scheduler
 from app.services.scheduler import schedule_reminder
+# redis
+from app.config.redis import redis
+from aioredis import Redis
 # other modules
 from typing import Any, Annotated
 import uuid
 
 router = APIRouter(prefix="/api/schedule", tags=["reminders"])
 
+"""
+Reminder Management Endpoints
+
+    The purpose of this docstring is following a DRY principle (don't repeat yourself)
+    by documenting common parameters and dependencies for endpoints in this file
+
+Common dependencies:
+    1. get_database: Provides an instance of AsyncIOMotorDatabase for database operations
+    2. get_current_user: Extracts and validates JWT token, retrieves user's details from the database
+
+Common Parameters:
+    1. db (AsyncIOMotorDatabase): Database connection instance
+    2. current_user (User): User that is performing this request with his JWT token
+"""
+
 
 @router.post("/reminder", response_model=ReminderResponse)
 async def create_reminder(
         reminder: CreateReminder,
-        task_id: uuid.UUID,
+        task_id: str,
+        current_user: Annotated[User, Depends(get_current_user)],
         db: Annotated[AsyncIOMotorDatabase, Depends(motor_db.get_database)],
-        current_user: Annotated[User, Depends(get_current_user)]
+        message_broker: Annotated[Redis, Depends(redis.get_redis())]
 ) -> Any:
-    collection = db.get_collection("reminders")
+    """Endpoint for creating a reminder for existing task
 
-    reminder_db = Reminder(
-        task_id=task_id,
+    Args:
+        reminder (CreateReminder): Data for the new reminder
+        task_id (str): Task ID for which reminder will be created
+        current_user (User): User that is performing this request with his JWT token
+        db (AsyncIOMotorDatabase): Database connection instance
+        message_broker (Redis): Redis connection instance
+
+    Returns:
+        ReminderResponse: Pydantic model with data added to Redis
+        (*)`Any` as a response type of the function is specified only for
+            the purpose of avoiding IDE warnings
+
+    Raises:
+        HTTPException (status_code=500): if new reminder cannot be inserted in the Redis
+
+    Dependency Functions:
+        see module-level docstring on top
+
+    Examples:
+        Request Body
+        {
+            "reminder_time": "2024-10-11:12:30",
+            "message": "Extremely important reminder, cannot be skipped"
+        }
+        Response Body
+        {
+            "reminder_time": "2024-10-11:12:30",
+            "message": "Extremely important reminder, cannot be skipped"
+        }
+    """
+    # structure of the new reminder in Message Broker - Reddis
+    reminder_data = Reminder(
+        task_id=uuid.UUID(task_id),
         user_id=current_user.id,
         user_email=current_user.email,
         reminder_time=reminder.reminder_time,
         message=reminder.message
     )
     try:
-        await collection.insert_one(reminder_db.model_dump())
+        # store reminder in Redis
+        await message_broker.set(str(reminder_data.task_id), reminder_data.model_dump_json())
+        # schedule the reminding email to be sent
+        await schedule_reminder(reminder_data, reminder_data.task_id, current_user, db)
+
         reminder_logger.info(
-            f"New reminder was successfully inserted in the database by user: {current_user.username}"
+            f"New reminder was successfully inserted in Reddis by user: {current_user.username}"
         )
     except Exception as e:
         reminder_logger.error(
-            f"Error inserting new reminder in the database by user: {current_user.username}"
+            f"Error inserting new reminder in Redis by user: {current_user.username}"
         )
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Error inserting new reminder in the database: {e}"
+            detail=f"Error inserting new reminder in Redis: {e}"
         )
 
-    # Schedule the reminder logic
-    await schedule_reminder(reminder.task_id, reminder.reminder_time)
-
-    reminder_logger.info("New reminder was successfully created")
-
-    return reminder_db
+    return reminder_data
 
 
-@router.get("/{reminder_id}", response_model=ReminderResponse)
-async def get_reminder(
-        reminder_id: uuid.UUID,
-        db: Annotated[AsyncIOMotorDatabase, Depends(motor_db.get_database)]
+@router.put("/reminder/{reminder_id}", response_model=ReminderResponse)
+async def update_reminder(
+        reminder_id: str,
+        reminder: UpdateReminder,
+        current_user: Annotated[User, Depends(get_current_user)],
+        message_broker: Annotated[Redis, Depends(redis.get_redis())]
 ) -> Any:
-    collection = db.get_collection("reminders")
-    reminder = await collection.find_one({"_id": reminder_id})
-    if not reminder:
-        raise HTTPException(status_code=404, detail="Reminder not found")
-    return Reminder(**reminder)
+    """Endpoint for updating an existing reminder
+
+    Args:
+        reminder_id (uuid.UUID): Reminder ID
+        reminder (CreateReminder): Data for updating reminder
+        current_user (User): User that is performing this request with his JWT token
+        message_broker (Redis): Redis connection instance
+
+    Returns:
+        ReminderResponse: Pydantic model with updated data
+        (*)`Any` as a response type of the function is specified only for
+            the purpose of avoiding IDE warnings
+
+    Raises:
+        HTTPException (status_code=404): if reminder with given ID was not found
+    """
+    existing_reminder = await message_broker.get(reminder_id)
+
+    if not existing_reminder:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Reminder was not found in Redis. Request by: {current_user.username}"
+        )
+
+    updated_reminder = Reminder(
+        task_id=existing_reminder.task_id,
+        user_id=existing_reminder.user_id,
+        user_email=existing_reminder.user_email if existing_reminder.user_email is not None else reminder.user_email,
+        reminder_time=existing_reminder.reminder_time if existing_reminder.reminder_time is not None else reminder.reminder_time,
+        message=existing_reminder.message if existing_reminder.messsage is not None else reminder.message
+    )
+    # TODO: Figure out how to re-schedule Celery tasks right
+    # check if we need to re-schedule Celery task
+    if reminder.reminder_time is not None:
+        pass
+
+    await message_broker.set(reminder_id, updated_reminder.model_dump_json())
+
+    return updated_reminder
 
 
-@router.delete("/{reminder_id}")
+@router.delete("/reminder/{reminder_id}", response_model=dict)
 async def delete_reminder(
-        reminder_id: uuid.UUID,
-        db: Annotated[AsyncIOMotorDatabase, Depends(motor_db.get_database)]
-) -> dict:
-    collection = db.get_collection("reminders")
-    result = await collection.delete_one({"_id": reminder_id})
-    if result.deleted_count == 0:
-        raise HTTPException(status_code=404, detail="Reminder not found")
-
-    return {"detail": f"Reminder (id: {reminder_id}) was successfully deleted"}
+        reminder_id: uuid.UUID
+):
+    pass
