469b215fd719df7b75206655dbd01162c12a4566
rastr <romanmilko123@gmail,com>
2025-07-11 13:40:41 +0200

    function for sending right reminding time

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,8 +1,17 @@
 # password hashing
 from passlib.context import CryptContext
+# time
+from datetime import datetime, timedelta
 
 myctx = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 
 def get_hashed_password(password: str) -> str:
     return myctx.hash(password)
+
+
+def set_reminding_time(deadline: datetime) -> datetime:
+    if deadline.time().hour == 0 and deadline.time().minute == 0:
+        return deadline
+    else:
+        return deadline - timedelta(hours=1)
